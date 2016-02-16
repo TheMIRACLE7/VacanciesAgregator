660fe684fff007867247af4b1881aea03db4b9c7
Sergey <s.s.miroshnichenko@mail.ru>
2016-02-16 10:08:31 +0300

    Corrected code to work with the new implementation of hh.ru pages

--- src/com/model/HHStrategy.java
+++ src/com/model/HHStrategy.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HHStrategy implements Strategy {
-    private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+    private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java&area=1&page=%d";
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
@@ -20,7 +20,7 @@ public class HHStrategy implements Strategy {
             int page = 0;
             while (true) {
                 Document doc = getDocument(searchString, page++);
-                Elements vacancyElements = doc.select("tr[data-qa=vacancy-serp__vacancy]");
+                Elements vacancyElements = doc.select("div[data-qa=vacancy-serp__vacancy]");
                 if (vacancyElements.size() > 0){
                     for (Element vacancyElement : vacancyElements) {
                         Vacancy v = new Vacancy();
@@ -32,7 +32,7 @@ public class HHStrategy implements Strategy {
                         v.setCity(vCity);
                         String vCompanyName = vacancyElement.select("a[data-qa=vacancy-serp__vacancy-employer]").text();
                         v.setCompanyName(vCompanyName);
-                        v.setSiteName("http://hh.ua/");
+                        v.setSiteName("http://hh.ru/");
                         String vURL = vacancyElement.select("[data-qa=vacancy-serp__vacancy-title]").attr("href");
                         v.setUrl(vURL);
                         list.add(v);
@@ -48,9 +48,9 @@ public class HHStrategy implements Strategy {
 
     protected Document getDocument(String searchString, int page) throws IOException{
         String userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36";
-        Document doc = Jsoup.connect(String.format(URL_FORMAT, searchString, page)).
+        Document doc = Jsoup.connect(String.format(URL_FORMAT, page)).
                     userAgent(userAgent).
-                    referrer("https://hh.ua/search/vacancy?text=java+%D0%BA%D0%B8%D0%B5%D0%B2")
+                    referrer("http://hh.ru/search/vacancy?text=Java&area=1")
                     .get();
         return doc;
     }
